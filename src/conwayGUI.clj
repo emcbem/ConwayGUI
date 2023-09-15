12ed6096175b35415d37216b7bdc28bc6dbf4aee
ethan.enger <ethan.enger@ENGR2KCP5S3>
2023-09-15 12:21:58 -0600

    Zoom out

--- src/conwayGUI.clj
+++ src/conwayGUI.clj
@@ -9,7 +9,7 @@
   (into #{} (remove (fn [cell] (= [x y] cell)) (for [i [-1 0 1] j [-1 0 1]] [(- x i) (- y j)]))))
 
 (defn num-living-neighbors [[x y] living]
-  (count (filter (fn [x] (not= nil x))  (for [i living] ((neighbors-of [x y]) i)))))
+  (count (filter (fn [x] (not= nil x))  (for [i (neighbors-of [x y])] (living i)))))
 
 (defn will-live? [[x y] living]
   (def neighbors (num-living-neighbors [x y] living))
@@ -20,6 +20,11 @@
   (let [living (into #{} state)]
     (into #{} (filter (fn [x] (will-live? x living)) (reduce clojure.set/union (into #{} (map neighbors-of living)))))))
 
+(defn rebase-2d-points [original-set]
+  (let [min-x (apply min (map first original-set))
+        min-y (apply min (map second original-set))]
+    (map #(vector (- (first %) min-x) (- (second %) min-y)) original-set)))
+
 
 (with-test (defn neighbors-of-test [[x y]] (neighbors-of [x y])) (is (= #{[1 1] [1 0] [1 -1] [0 1] [0 -1] [-1 1] [-1 0] [-1 -1]} (neighbors-of-test [0 0]))))
 (with-test (defn num-living-neighbors-test [cell living] (num-living-neighbors cell living)) (is (= 3 (num-living-neighbors-test [0 0] #{[0 1] [1 0] [-1 -1] [6 9]}))))
@@ -28,24 +33,36 @@
 (defn tests [opts]
   (run-all-tests))
 
-
 (defn setup []
-  (q/frame-rate 100)
+  (q/frame-rate 30)
   (def color [(q/random 255) (q/random 255) (q/random 255)])
-  (remove nil? (into #{}  (for [i (range 40) j (range 40)] (if (< (rand 1) 0.1) [i j])))))
+  ;(def background-color [(- 255 (nth color 0)) (- 255 (nth color 1)) (- 255 (nth color 2))])
+  {:livingList (into #{} (remove nil? (for [i (range 40) j (range 40)] (if (< (rand 1) 0.1) [i j])))),
+   :xRatio 10,
+   :yRatio 10})
+
+(defn update [state] 
+  (let [newXRatio (/ (q/width) (* 1.25 (apply max (map first (state :livingList))))),
+        newYRatio (/ (q/height) (* 1.25 (apply max (map second (state :livingList)))))]
+  (assoc-in (assoc-in  (assoc-in state [:yRatio] newYRatio)  [:xRatio] newXRatio) [:livingList] (rebase-2d-points(next-generation-living (:livingList state)))))
+  )
+
 
 (defn draw [old-state]
-  (q/background 220 224 221)
-  (doseq [i old-state] ((q/fill (first color) (second color) (nth color 2)) (q/stroke 80 0 255) (q/rect (* 10 (first i)) (* 10 (second i)) 10 10))))
+  (q/background 220 221 224)
+  (doseq [i (:livingList old-state)] ((q/fill (first color) (second color) (nth color 2))
+                                      (q/stroke 80 0 255)
+                                      (q/rect (* (:xRatio old-state) (first i)) (* (:yRatio old-state) (second i)) (:xRatio old-state) (:yRatio old-state)))))
 
-(q/defsketch conway
-  :title "Conway Game of LIfe"
-  :setup #'setup
-  :size [500 500]
-  :update #'next-generation-living
-  :draw #'draw
-  :middleware [m/fun-mode])
 
-(defn -main [& args])
+(defn -main [& args]
+  (q/defsketch conway
+    :title "Conway Game of LIfe"
+    :setup #'setup
+    :size [500 500]
+    :update #'update
+    :draw #'draw
+    :middleware [m/fun-mode])
+  )
 
 
